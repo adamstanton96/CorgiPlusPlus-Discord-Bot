bf081be1f28f8315fae60daad150d36308b06e24
Adam Stanton <a.stanton.1996@gmail.com>
2018-04-19 09:16:06 +0100

    Bot Initialized (Issues Present)
    Issues surrounding the windows 7 OS - No websockets support. Will attempt further work on another platform for now until a solution is found.

--- CorgiBot/Program.cs
+++ CorgiBot/Program.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +14,67 @@ namespace CorgiBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult(); //Mimics async functionality within static method.
+
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        public async Task RunBotAsync()
+        { 
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+
+            string botToken = "NDM2Mjc4MzQzODE4NTQzMTE1.DbnYgA.IKRbLRnCHRPJi7Q-IwOy0k0XNJo";
+
+            //Event subscription:
+            client.Log += Log;
+
+            await RegisterCommandAsync();
+            await client.LoginAsync(TokenType.Bot, botToken);
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
         {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot)
+            {
+                return;
+            }
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("Corgibot! ", ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(client, message);
+                var result = await commands.ExecuteAsync(context, argPos, services);
+
+                if(!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
         }
     }
 }
